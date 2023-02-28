f22023be3ae005b928502d6c5069278e36e4ee8a
Touheed Razaq <trkraza0204@gmail.com>
2023-02-27 21:55:13 -0800

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Code from './Code'
+// import Header from './Header'
+import Header from './Header'
+import Result from './Result'
+
+
+const Home = () => {
+  return (
+    <>
+     
+        <Header/>
+        <Code/>
+        <Result/>
+        
+        </>
+  )
+}
+
+export default Home
+
+
+
